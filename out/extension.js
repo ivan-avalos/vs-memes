cc50a66191f87fe88ab15743da92df6bec52e152
avalos <ivan.avalos.diaz@hotmail.com>
2019-07-10 23:36:25 -0500

    Added icon to extension in package.json.

--- out/extension.js
+++ out/extension.js
@@ -1,4 +1,21 @@
 "use strict";
+/*
+ * Visual Studio Code Memes
+ * Copyright (C) 2019  Iván Ávalos <ivan.avalos.diaz@hotmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 const axios_1 = require("axios");
